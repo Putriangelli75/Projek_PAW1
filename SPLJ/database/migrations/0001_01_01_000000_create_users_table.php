372a15b0d32f4118caab7633143e0b1b7aa637fb
Putriangelli75 <putriangelli75@gmail.com>
2026-06-03 20:45:06 +0700

    tambah migrations dan models

--- SPLJ/database/migrations/0001_01_01_000000_create_users_table.php
+++ SPLJ/database/migrations/0001_01_01_000000_create_users_table.php
@@ -34,7 +34,9 @@ return new class extends Migration
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
+
         });
+        
     }
 
     /**
